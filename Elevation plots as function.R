3482c462aed25fcf8a3cf0ba558d42872a2cca86
EPdeJ <erikpieterdejong@gmail.com>
2025-01-06 16:14:24 +0900

    Update, function works

--- Elevation plots as function.R
+++ Elevation plots as function.R
@@ -20,47 +20,37 @@
 
 
 # load packages and filepaths ---------------------------------------------
-pacman::p_load(tidyverse,sf,zoo,Cairo)
+pacman::p_load(tidyverse,sf,zoo,Cairo, scales)
 S.path <- "G:/.shortcut-targets-by-id/1kT69UY4d-Ny3cmezFuDPbeQRMwDT32dn/Fietsboek/2025/gpx"
 S.gpxlist <- list.files(path = S.path, pattern = "\\.gpx$", full.names = T)
-gpx <- st_read(S.gpxlist[1],layer="track_points") 
 
-#!!!!!!!!!!!!temp vars for development (remove in final version)!!!!!!!!!!!!!!!!!
-seq=10
-roll=10
-
-transparency=1
-colorscalestr=c("#9198A7","#C9E3B9", "#F9D49D", "#F7B175", "#F47D85", "#990000")
-rollparameter="max"
-linecolor="#23b09d"
-maxlinecol="red"
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 # function elevation profile -------------------------------------------------------
 # set start parameters for function
 elevationprofile <- function(filepath,
+                             seq=10,
                              roll=10,
                              rollparameter="max",
-                            
                              colorscalestr=c("#9198A7","#C9E3B9", "#F9D49D", "#F7B175", "#F47D85", "#990000"),
                              linecolor="#23b09d",
                              maxlinecol="red",
                              transparency=1,
-                              
                              plotsave=F,
-                             plotname="empty",
-                             ggsavepar=c(24,10,"cm",300),
-                             seq=F){
+                             plotsavedir="",
+                             plotname="",
+                             ggsave_width=24,
+                             ggsave_height=10,
+                             ggsave_dpi=300,
+                             ggsave_units="cm",
+                             ggsave_background="transparent"
+                             ){
                       
                     
 # import gpx into sf object to add geometry
 gpx <- st_read(filepath,layer="track_points") #turn into sf object
 gpx <- gpx %>% select(track_seg_point_id, ele,geometry) #select the usefull rows
-
-# WORK IN PROGRESS (if seq is defined it will reduce the number of datapoints from the gpx)
-if(seq){gpx <- gpx %>% slice(c(1, seq(seq, nrow(gpx)-1, seq), nrow(gpx)))}
-
 
 # calculate the difference in distance and elevation between data points
 gpx<- gpx %>%
@@ -76,6 +66,15 @@ gpx<- gpx %>%
     elevation_total = cumsum(ifelse(elevation_diff > 0, elevation_diff, 0)),
     gradient=(elevation_diff/distance_diff)*100
         ) # elevation can be negative so only cum sum when positive 
+
+# WORK IN PROGRESS (if seq is defined it will reduce the number of datapoints from the gpx)
+if(seq){
+  max_ele_index <- which.max(gpx$ele)
+  n_rows <- nrow(gpx)
+  intermediate_rows <- seq(from = 1 + seq, to = n_rows - 1, by = seq)
+  rows_to_select <- unique(sort(c(1, max_ele_index, n_rows, intermediate_rows)))
+  gpx <- gpx %>% slice(rows_to_select)
+}
 
 # calculate rolling averages
 gpx<- gpx %>% 
@@ -131,13 +130,69 @@ gpx <- gpx %>%
 gpx <- gpx %>% st_drop_geometry()
 mem <- gpx
 
-# find maximum elevation (highest point) and max distance
+# find maximum elevation (highest point) & max distance
 maxele <- max(gpx$ele)
+maxeledist <- gpx %>% filter(ele==maxele) %>% select(distance_total) %>% pull/1000
 maxdist <- max(gpx$distance_total)
+
+# calculate the position for three lines in between the max line and the x axis (y=0)
+rib0ele <- min(gpx$ele)
+rib0dist <- gpx %>%  mutate(difference = abs(ele - rib0ele)) %>%  
+  slice_min(difference) %>% select(distance_total) %>% pull/1000
+rib1ele <- (maxele-rib0ele)/4
+rib1dist <- gpx %>%  mutate(difference = abs(ele - rib1ele)) %>%  
+  slice_min(difference) %>% select(distance_total) %>% pull/1000
+rib2ele <- rib1ele*2
+rib2dist <- gpx %>%  mutate(difference = abs(ele - rib2ele)) %>%  
+  slice_min(difference) %>% select(distance_total) %>% pull/1000
+rib3ele <- rib1ele*3
+rib3dist <- gpx %>%  mutate(difference = abs(ele - rib3ele)) %>%  
+  slice_min(difference) %>% select(distance_total) %>% pull/1000
 
 
 # make the plot
-ggplot(data=gpx)+
+plot <- ggplot(data=gpx)+
+  
+  #add max height line
+  annotate("segment",
+           x=maxeledist,
+           xend=maxdist/1000,
+           y=maxele,
+           yend=maxele,
+           colour=maxlinecol,
+           linetype = 'dashed',
+           linewidth= .5)+
+  
+  #add 3 even lines inbetween max line and lowest elevation
+  annotate("segment",
+           x=rib1dist,
+           xend=maxdist/1000,
+           y=rib1ele,
+           yend=rib1ele,
+           colour="lightgrey",
+           linewidth= .5)+
+  annotate("segment",
+           x=rib2dist,
+           xend=maxdist/1000,
+           y=rib2ele,
+           yend=rib2ele,
+           colour="lightgrey",
+           linewidth= .5)+
+  annotate("segment",
+           x=rib3dist,
+           xend=maxdist/1000,
+           y=rib3ele,
+           yend=rib3ele,
+           colour="lightgrey",
+           linewidth= .5)+ 
+  annotate("segment",
+           x=rib0dist,
+           xend=maxdist/1000,
+           y=rib0ele,
+           yend=rib0ele,
+           colour="lightgrey",
+           linewidth= .5)+
+  
   #plot "downhill or flat" area under the curve, which serves as the background
   geom_ribbon(aes(x=distance_total/1000,ymax=ele,ymin=0),
               fill=colorscalestr[1],
@@ -220,10 +275,26 @@ ggplot(data=gpx)+
   #' the set -1000000 for the shades for the gradients. This was needed because a 
   #' point with an elevation connecting to 0 would create a sloped line instead of a 
   #' vertical line. This still needs to be solved but the -1000000 patch is visually working
-  scale_y_continuous(limits = c(0,maxele))+
+  scale_y_continuous(limits = c(0,maxele+20),  position = "right",
+                     breaks = c(rib0ele, rib1ele,rib2ele,rib3ele,maxele),
+                     labels = c(paste0(round(rib0ele,0), " m"), 
+                                paste0(round(rib1ele,0), " m"), 
+                                paste0(round(rib2ele,0), " m"),
+                                paste0(round(rib3ele,0), " m"),
+                                paste0(round(maxele,0), " m \n(Highest Point)")),
+                     expand = c(0,0)
+                     )+ 
+  scale_x_continuous(
+    breaks = c(seq(0, maxdist/1000, by = 10),maxdist/1000), 
+    labels = paste0(c(seq(0, maxdist/1000, by = 10),round(maxdist/1000,0)), " Km"), 
+    minor_breaks = NULL,
+    expand = c(0,1)
+  ) +
 
+
+  
   #add height profile line
-  geom_line(aes(x=distance_total/1000,y=ele), colour= linecolor, size=1, lineend="round")
+  geom_line(aes(x=distance_total/1000,y=ele), colour= linecolor, linewidth=1, lineend="round")+
   
   #add start and end point
   geom_point(data=. %>% slice(c(1, n())),
@@ -232,59 +303,51 @@ ggplot(data=gpx)+
                y=ele),
              size = 4, # Adjust size as needed
              shape = 21, # Shape 21 is a circle with a fill and stroke
-             fill = "red", # Fill color
+             fill = "transparent", # Fill color
              color = linecolor, # Stroke color
              stroke = 1, # Stroke thickness
-             alpha = 0.5) # Transparency
-
-  
-  #add max height line
-  geom_hline(aes(yintercept=max(ele)),linetype = 'dashed', col = maxlinecol)+
+             alpha = 1)+ # Transparency
   
   #change labels x and y axis
   xlab("Distance (km)")+
   ylab("Elevation (m)")+
   
-  #modify the ticks and place of y axis
-  scale_y_continuous(position = "right", limits = c(0,3500), expand = c(0,0))+
-  
   #further add changes to layout
   theme(panel.grid.minor = element_blank(), 
         panel.grid.major = element_blank(),
-        # axis.ticks= element_blank(),
         panel.background = element_rect(fill = "transparent",color = NA), 
         plot.background = element_rect(fill = "transparent", color = NA),
-        axis.text.x = element_text(vjust = .5, margin = margin(-0.5,0,0.5,0, unit = 'cm')),
-        axis.ticks.length=unit(-0.25, "cm"),
-        axis.title.x = element_text(vjust = -2, margin = margin(-0.5,0,0.5,0, unit = 'cm'))
+        axis.text.x = element_text(vjust = .5, margin = margin(t = 3)),
+        axis.ticks.length.x = unit(-0.15, "cm"),
+        axis.ticks.x = element_line(color = "lightgrey", linewidth=1.5),
+        axis.ticks.y = element_blank(),
+        axis.title = element_blank()
+        )
          
-  )
-
-suppressWarnings(print(plot))
+  
 
 if(plotsave){suppressMessages(ggsave(plot= plot,
-                    paste0(plotname,".png"),
-                    width = as.numeric(ggsavepar[1]),
-                    height = as.numeric(ggsavepar[2]),
-                    units =ggsavepar[3],
-                    dpi = as.numeric(ggsavepar[4]),
-                    type = "cairo-png",
-                    bg = "transparent" ))}
+                                     path = plotsavedir,
+                                     filename = paste0(ifelse(plotname!="", paste(plotname), sub("\\.gpx$", "", basename(filepath))),".png"),
+                                     width = as.numeric(ggsave_width),
+                                      height = as.numeric(ggsave_height),
+                                      units =ggsave_units,
+                                      dpi = as.numeric(ggsave_dpi),
+                                      type = "cairo-png",
+                                      bg = ggsave_background ))
+  }
 
 plot
+
 }
 
 
 
 # use function ------------------------------------------------------------
-elevationprofile(S.gpxlist[1],plotname = "Wufenshan test", gpxrolling = 200, plotsave = F , seq=F)
+elevationprofile(S.gpxlist[1],
+                 plotsavedir = "C:/Users/erik6/OneDrive/Desktop/test", 
+                 roll = 7, 
+                 seq=10,
+                 plotsave = F,  
+                 rollparameter="mean")
 
- elevationprofile("gpx/crazy ride.gpx",                            #set filepath including .gpx
-                 gpxrolling=50,                                   #set roling parameter, standard value is 10
-                 linecolor="red",                                 #color of elevation profile line
-                 maxlinecol="green",                              #color of the max line
-                 transparency=.7,                                 #set transparency
-                 plotsave=T,                                      #save plot in wd
-                 plotname="Test",                                 #Name of plot to save
-                 ggsavepar=c(10,10,"cm",150)       #dementions of plot to save, unit and dpi's
-)
